6a4657258e7a51db5a1ada3d643525e86afea604
Yash Bhayani <yashbhayani3007@gmail.com>
2022-08-05 14:08:07 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGx5Zr1LrTQ4wkKpkYrAx819moOrCkzQU",
+    authDomain: "angular-crud-de9ce.firebaseapp.com",
+    databaseURL: "https://angular-crud-de9ce-default-rtdb.firebaseio.com",
+    projectId: "angular-crud-de9ce",
+    storageBucket: "angular-crud-de9ce.appspot.com",
+    messagingSenderId: "1035272140434",
+    appId: "1:1035272140434:web:f7338cdde0958f6777584b"
+  }
+};
