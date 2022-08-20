2dce69e4ac44db10b4105e1f4beebad4852410f5
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2022-08-20 22:48:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDGx5Zr1LrTQ4wkKpkYrAx819moOrCkzQU",
-    authDomain: "angular-crud-de9ce.firebaseapp.com",
-    databaseURL: "https://angular-crud-de9ce-default-rtdb.firebaseio.com",
-    projectId: "angular-crud-de9ce",
-    storageBucket: "angular-crud-de9ce.appspot.com",
-    messagingSenderId: "1035272140434",
-    appId: "1:1035272140434:web:f7338cdde0958f6777584b"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_authDomain",
+    projectId: "YOUR_projectId",
+    storageBucket: "YOUR_storageBucket",
+    messagingSenderId: "YOUR_messagingSenderId",
+    appId: "YOUR_appId"
   }
 };
